bbb9c7af9cdf44fd506968bdf32ae450259decdc
pseweryn <piotr.seweryn@coi.gov.pl>
2024-03-18 15:14:04 +0100

    imports

--- app/src/main/java/com/seweryn/piotr/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/seweryn/piotr/codingchallenge/MainActivity.kt
@@ -7,12 +7,8 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.seweryn.piotr.codingchallenge.presentation.salesmanlist.SalesmanListScreen
-import com.seweryn.piotr.codingchallenge.presentation.salesmanlist.SalesmanListViewModel
 import com.seweryn.piotr.codingchallenge.presentation.salesmanlist.SalesmanListViewModelImpl
 import com.seweryn.piotr.codingchallenge.ui.theme._1und1CodingChallengeTheme
 
--- app/src/main/java/com/seweryn/piotr/codingchallenge/presentation/salesmanlist/SalesmanListScreen.kt
+++ app/src/main/java/com/seweryn/piotr/codingchallenge/presentation/salesmanlist/SalesmanListScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -93,7 +91,7 @@ private fun SalesmanListItem(
     Icon(
       modifier = Modifier.padding(16.dp),
       painter = painterResource(
-        id = if(data.expanded) {
+        id = if (data.expanded) {
           R.drawable.ic_chevron_down
         } else {
           R.drawable.ic_chevron_right
