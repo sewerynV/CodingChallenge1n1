d24a952ea4ce0dedaa986a09320bdc9c95f79928
pseweryn <piotr.seweryn@coi.gov.pl>
2024-03-18 16:22:25 +0100

    salesman filter

--- domain/src/main/java/com/seweryn/piotr/domain/SalesmanFilter.kt
+++ domain/src/main/java/com/seweryn/piotr/domain/SalesmanFilter.kt
@@ -5,3 +5,13 @@ import com.seweryn.piotr.domain.model.Salesman
 internal interface SalesmanFilter {
   fun filter(list: List<Salesman>, area: String): List<Salesman>
 }
+
+internal class SalesmanFilterImpl : SalesmanFilter {
+  override fun filter(list: List<Salesman>, area: String): List<Salesman> =
+    list.filter { salesman ->
+      salesman.areas.any { area ->
+        area.startsWith(area)
+      }
+    }
+
+}
